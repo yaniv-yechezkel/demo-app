cd60e5aa4b5f1df60e576ba472c0a01db22f003a
Yaniv <yaniv@upstruct.net>
2020-12-19 22:55:02 +0200

    Initial Commit

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DemoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($"Hello from {_configuration["REGION"]}");
+        }
+    }
+}
